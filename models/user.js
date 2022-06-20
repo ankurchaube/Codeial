ce5109c5adb1929eb7d983873d4cc3560c2e1e54
Ankur Chaubey <you@example.com>
2022-06-20 14:10:33 +0530

    Adding Files

--- models/user.js
+++ models/user.js
@@ -18,13 +18,32 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
-    }       
+    },
+    
+    avtar: {
+        type: String
+    }
     
 }, {
     timestamps: true
 });
 
+let storage = multer.diskStorage({
+    destination: function(req,file, cb){
+      cb (null, path.join(__dirname, '..', AVTAR_PATH)); 
+
+    },
+    filename:  function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now());
+    }
+});
+
+//  static
+
+userSchema.statics.uploadedAvtar = multer({storage: storage}).single('avtar');
+
 const User = mongoose.model('User', userSchema);
+userSchema.statics.avtarPath = AVTAR_PATH;
 
 module.exports = User;
 
