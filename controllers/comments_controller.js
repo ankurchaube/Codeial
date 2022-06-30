d6a184432cbed1ec4464c6df94090c2daec469fd
Ankur Chaubey <you@example.com>
2022-06-30 20:03:21 +0530

    implementing queue

--- controllers/comments_controller.js
+++ controllers/comments_controller.js
@@ -1,5 +1,9 @@
 const Comment = require('../models/comment');
 const Post = require('../models/post');
+const commentsMailer = require('../mailers/comments_mailers');
+const queue = require('../config/kue');
+const commentEmailWorker = require('../workers/comment_email_worker');
+
 
 module.exports.create = async function(req, res){
 
@@ -15,11 +19,37 @@ module.exports.create = async function(req, res){
 
             post.comments.push(comment);
             post.save();
+            
+            comment = await comment.populate('user', 'name email');
+            // commentsMailer.newComment(comment);
+
+            let job = queue.create('emails', comment).save(function(err){
+                if (err){
+                    console.log('Error in sending to the queue', err);
+                    return;
+                }
+                console.log('job enqueued', job.id);
+
+            })
+
+            if (req.xhr){
+                
+    
+                return res.status(200).json({
+                    data: {
+                        comment: comment
+                    },
+                    message: "Post created!"
+                });
+            }
+
+
+            req.flash('success', 'Comment published!');
 
             res.redirect('/');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
@@ -30,7 +60,6 @@ module.exports.destroy = async function(req, res){
 
     try{
         let comment = await Comment.findById(req.params.id);
-       
 
         if (comment.user == req.user.id){
 
@@ -40,12 +69,26 @@ module.exports.destroy = async function(req, res){
 
             let post = Post.findByIdAndUpdate(postId, { $pull: {comments: req.params.id}});
 
+            // send the comment id which was deleted back to the views
+            if (req.xhr){
+                return res.status(200).json({
+                    data: {
+                        comment_id: req.params.id
+                    },
+                    message: "Post deleted"
+                });
+            }
+
+
+            req.flash('success', 'Comment deleted!');
+
             return res.redirect('back');
         }else{
+            req.flash('error', 'Unauthorized');
             return res.redirect('back');
         }
     }catch(err){
-        console.log('Error', err);
+        req.flash('error', err);
         return;
     }
     
